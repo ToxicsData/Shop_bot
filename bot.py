1781a41be0d915a8faeefd493143b1bff5c68e73
Whiteroad95 <zhanabayevakzhol@gmail.com>
2022-12-05 21:35:43 +0600

    first commit

---
+++ bot.py
@@ -0,0 +1,112 @@
+import telebot
+from telebot import types
+
+from config import TOKEN
+from main import insert_user, is_user_exists, create_inline_markup
+from location import insert_location, is_location_exists, update_location
+from products import get_brands, get_phone_callbacks, get_info_phone, send_phone
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message: types.Message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    item1 = types.KeyboardButton('Зарегистрироваться', request_contact=True)
+    item2 = types.KeyboardButton('Отправить геоданные', request_location=True)
+    markup.add(item1, item2)
+
+    text = "Привет это Бот Мамина подруги \n Для получения данных ЗАРЕГИСТРИРУЙТЕСЬ"
+    bot.send_message(message.chat.id, text=text, reply_markup=markup)
+
+
+@bot.message_handler(content_types=['contact'])
+def contact(message: types.Message):
+    if message.contact is not None:
+
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        item1 = types.KeyboardButton('Продукты')
+        item2 = types.KeyboardButton('Корзина')
+        markup.add(item1, item2)
+
+        if not is_user_exists(message.chat.id):
+            insert_user(
+                phone_number=message.contact.phone_number,
+                first_name=message.contact.first_name,
+                last_name=message.contact.last_name,
+                chat_id=message.chat.id
+            )
+            bot.send_message(message.chat.id, 'Вы успешно зарегистровались!', reply_markup=markup)
+        else:
+            bot.send_message(message.chat.id, 'Вы уже зарегистрованы!', reply_markup=markup)
+
+@bot.message_handler(content_types=['location'])
+def location(message: types.Message):
+    if message.location is not None:
+        if not is_location_exists(message.chat.id):
+            insert_location(
+                chat_id=message.chat.id,
+                latitude=message.location.latitude,
+                longitude=message.location.longitude
+            )
+            bot.send_message(message.chat.id, 'Вы успешно отправили геоданные!')
+        else:
+            update_location(
+                chat_id=message.chat.id,
+                latitude=message.location.latitude,
+                longitude=message.location.longitude
+            )
+            bot.send_message(message.chat.id, 'Вы успешно обновили геоданные!')
+
+            
+
+@bot.message_handler(content_types=['text'])
+def text(message: types.Message):
+    if message.chat.type == 'private':
+        if message.text.lower() == 'продукты':
+            brands = get_brands()
+            markup = create_inline_markup(row_width=3, **brands)
+            bot.send_message(message.chat.id, 'Выберите свой желаемый бренд:', reply_markup=markup)
+
+            
+@bot.callback_query_handler(func=lambda call: True)
+def callback_handler(call):
+    try:
+        if call.message:
+            if call.data in get_brands():
+                phones = get_phone_callbacks(call.data)
+                markup = create_inline_markup(
+                    row_width=3,
+                    **phones,
+                    products='🔙 Назад')
+                bot.edit_message_text(
+                    chat_id=call.message.chat.id,
+                    message_id=call.message.message_id,
+                    text='Выберите модель:',
+                    reply_markup=markup
+                )
+            elif call.data == 'products':
+                """продукты call back """
+                markup = create_inline_markup(
+                        row_width=3, 
+                        **get_brands())
+                bot.edit_message_text(
+                    chat_id=call.message.chat.id,
+                    message_id=call.message.message_id,
+                    text='Выберите свой желаемый Бренд:',
+                    reply_markup=markup
+                )
+            elif call.data in get_phone_callbacks():
+                phones = get_info_phone(call.data)
+                for phone in phones:
+                    send_phone(
+                        call=call,
+                        bot=bot,
+                        phone=phone)
+                   
+    except: 
+        pass
+            
+                
+
+
+bot.polling(non_stop=True)
---
+++ parse_products.py
@@ -0,0 +1,93 @@
+from bs4 import BeautifulSoup
+import requests 
+from config import HEADERS, URL, DOMEN, LIST_BRANDS
+from products import insert_product
+
+# id | name | description | price | photo | memory | color | brand | call_back | url
+
+
+def get_html(URL, headers=HEADERS, params=' '):
+    html = requests.get(URL, headers=headers, params=params)
+    soup = BeautifulSoup(html.text, 'html.parser')
+    return soup
+
+
+def get_data(soup, brand):
+    items = soup.find_all('div', class_='item-card')
+    data = []
+    for item in items:
+        try:
+            name = item.find('div', class_='item-card__info')\
+                .find('a').get_text(strip=True).split()
+            right_name = ''
+            memory = ''
+            color = name[-1]
+            call_back = '_'.join(name[:3]).lower()
+            for i in name[:-1]:
+                if 'gb' in i.lower() or 'гб' in i.lower():
+                    memory += i
+                else:
+                    right_name += i+' '
+        except:
+            right_name = None
+            memory = None
+            color = None
+            call_back = None
+        try:
+            url = DOMEN + item.find('a', class_='item-card__image-wrapper').get('href')
+        except:
+            url = None
+
+        try:
+            price = item.find('span', class_='item-card__prices-price').get_text(strip=True).replace('₸', '').replace(' ','')
+        except:
+            price = 0
+        local_data = get_html(url)
+        img = local_data.find_all('meta')[12].get('content')
+        description = local_data.find('div', class_='item__description-text').get_text(strip=True)
+        
+        if right_name is not None:
+            data.append({
+                'name': right_name,
+                'description': description,
+                'price': int(price),
+                'photo': img,
+                'memory': memory,
+                'color': color,
+                'brand': brand,
+                'call_back': call_back,
+                'url': url
+            })
+    
+    return data  
+        
+
+def parse(LIST_BRANDS, page):
+    contents = []
+    for brand in LIST_BRANDS:
+        for page in range(1 , page+1):
+            html = get_html(URL + brand, params={'page': page})
+            content = get_data(html, brand)
+            contents.extend(content)
+            print(f"Страница Бренд {brand} {page} готово!")
+    return contents
+
+def add_products_db(items: list[dict]):
+    for item in items:
+        insert_product(
+            name=item['name'],
+            description=item['description'],
+            price=item['price'],
+            photo=item['photo'],
+            memory=item['memory'],
+            color=item['color'],
+            brand=item['brand'],
+            call_back=item['call_back'],
+            url=item['url'])
+        print(f"{item['name']} добавлен в базу данных!")
+
+
+
+
+# items = parse(LIST_BRANDS, page=2)
+# add_products_db(items=items)
